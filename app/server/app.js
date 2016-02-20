40b0d18b9466928883b2c638005fb5401027991d
Marc Vornetran <marc.vornetran@gmail.com>
2016-02-20 16:57:01 +0100

    MongoDB connection via Mongoose

--- app/server/app.js
+++ app/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const mongoose = require('mongoose');
 
 app.use(express.static('public'));
 
@@ -10,3 +11,13 @@ server.listen(8000);
 io.on('connection', socket => {
     socket.emit('hello world', { hello: 'Greetings!' });
 });
+
+mongoose.connect('mongodb://localhost/clash-of-flags');
+
+const db = mongoose.connection;
+
+db.on('error', err => console.error(err));
+
+db.once('open', () => {
+    console.log('Connected to MongoDB!');
+})
