e221a91802acbf0a41fca0b10d8319caff8232ee
Marc Vornetran <marc.vornetran@gmail.com>
2016-03-01 10:36:40 +0100

    Refactor client api module

--- app/client/api/index.js
+++ app/client/api/index.js
@@ -2,13 +2,6 @@ import register from './register';
 import login from './login';
 
 export default {
-
-    register(email, username, password) {
-        return register(email, username, password);
-    },
-
-    login(username, password) {
-        return login(username, password);
-    }
-
+    register,
+    login
 };
