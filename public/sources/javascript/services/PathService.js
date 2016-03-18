bc3edffaeee87dc358cc15f60b0b40b2f55f0aa9
Jochen Walther <waltherjochen@web.de>
2016-03-18 11:05:51 +0100

    Refactor image folder

--- public/sources/javascript/services/PathService.js
+++ public/sources/javascript/services/PathService.js
@@ -3,7 +3,35 @@ export default class PathService {
 
     }
 
-    image(imageName) {
-        return 'assets/images/' + imageName;
+    imageWeapon(imageName) {
+      return 'assets/images/weapon/' + imageName;
+    }
+
+    imageAnimation(imageName) {
+      return 'assets/images/animation/' + imageName;
+    }
+
+    imageItem(imageName) {
+      return 'assets/images/items/' + imageName;
+    }
+
+    imageMap(imageName) {
+      return 'assets/images/map/' + imageName;
+    }
+
+    imageOthers(imageName) {
+      return 'assets/images/others/' + imageName;
+    }
+
+    imagePlayer(imageName) {
+      return 'assets/images/player/' + imageName;
+    }
+
+    imageTilset(imageName) {
+      return 'assets/images/tilset/' + imageName;
+    }
+
+    tilemaps(imageName) {
+      return 'assets/tilemaps/' + imageName;
     }
 }
