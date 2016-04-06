180c231e50c38d84538f6e66c405cda36db22bd7
Jochen Walther <waltherjochen@web.de>
2016-04-06 12:20:20 +0200

    Implement Jochen's cool homepage

--- public/sources/js/client/components/views/About.vue
+++ public/sources/js/client/components/views/About.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <h1 class="display-4">About Clash of Flags</h1>
             <p class="lead">
-                This project has been created by <strong><a href="http://schneider.click" target="_blank">Simon Schneider</a></strong>, <strong><a href="http://schemel.me" target="_blank">Philipp Schemel</a></strong>, <strong>Jochen Walther</strong> & <strong><a href="http://marc.vornetran.de" target="_blank">Marc Vornetran</a></strong> for the <i>"Games and Gaming"</i> course at the <strong>Cooperative State University</strong> in <strong>Karlsruhe</strong>.</p>
+                This project has been created by <strong><a href="http://schneider.click" target="_blank">Simon Schneider</a></strong>, <strong><a href="http://schemel.me" target="_blank">Philipp Schemel</a></strong>, <strong><a href="http://waltherjochen.de" target="_blank">Jochen Walther</a></strong> & <strong><a href="http://marc.vornetran.de" target="_blank">Marc Vornetran</a></strong> for the <i>"Games and Gaming"</i> course at the <strong>Cooperative State University</strong> in <strong>Karlsruhe</strong>.</p>
             </p>
         </div>
     </div>
