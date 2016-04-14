f88093cd3d2eb3508c13633bf5f5adf50322fd57
Simon Schneider <simon@schneider.click>
2016-04-14 16:06:28 +0200

    Started with statistics stuff

---
+++ public/sources/js/client/components/views/Statistics.vue
@@ -0,0 +1,34 @@
+<template>
+    <br>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4 center-block">
+            <canvas id="chart__kills" width="400" height="400"></canvas>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+    import api from '../../api';
+
+    export default {
+        data() {
+            return {};
+        },
+        computed: {},
+        route: {
+            activate() {
+                jQuery(function ($) {
+                    var ctx = document.getElementById("chart__kills").getContext("2d");
+                    api.statistics()
+                            .then(result => {
+                                var baseChart = new Chart(ctx, {
+                                    type: 'line',
+                                    data: result.kills
+                                });
+
+                            });
+                });
+            }
+        }
+    }
+</script>
