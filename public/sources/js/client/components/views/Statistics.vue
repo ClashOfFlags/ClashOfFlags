8588f90c854f747e424aa4a2757af5854ecc309d
Simon Schneider <simon@schneider.click>
2016-04-14 16:53:32 +0200

    Started with statistics stuff

--- public/sources/js/client/components/views/Statistics.vue
+++ public/sources/js/client/components/views/Statistics.vue
@@ -2,7 +2,7 @@
     <br>
     <div class="row">
         <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4 center-block">
-            <canvas id="chart__kills" width="400" height="400"></canvas>
+            <div id="chart__kills"></div>
         </div>
     </div>
 </template>
@@ -18,12 +18,34 @@
         route: {
             activate() {
                 jQuery(function ($) {
-                    var ctx = document.getElementById("chart__kills").getContext("2d");
                     api.statistics()
                             .then(result => {
-                                var baseChart = new Chart(ctx, {
-                                    type: 'line',
-                                    data: result.kills
+                                var data = [
+                                    {'date': new Date('2015-04-01'), 'value': 12},
+                                    {'date': new Date('2015-04-02'), 'value': 18},
+                                    {'date': new Date('2015-04-010'), 'value': 98}
+                                ];
+
+                                console.log(result);
+
+                                var kills = [];
+                                for (var i = 0; i < result.kills.length; i++) {
+                                    kills[i] = MG.convert.date(result.kills[i], 'date');
+                                }
+
+                                console.log(kills);
+
+                                MG.data_graphic({
+                                    title: "Kills",
+                                    description: "Yearly UFO sightings from 1945 to 2010.",
+                                    data: kills,
+                                    width: 600,
+                                    height: 400,
+                                    target: "#chart__kills",
+                                    x_accessor: "date",
+                                    y_accessor: "value",
+                                    legend: ['Kills Red','Kills Blue'],
+                                    interpolate: "monotone"
                                 });
 
                             });
