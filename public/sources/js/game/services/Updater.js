95331ff2d7d1344495d3f3ad9148c7b573d327ad
schempil <PhilippSchemel@live.com>
2016-04-14 14:42:02 +0200

    Keys can now be returned to the base (where all the gold is located)

--- public/sources/js/game/services/Updater.js
+++ public/sources/js/game/services/Updater.js
@@ -98,7 +98,7 @@ export default class Updater {
         this.game.physics.arcade.collide(this.barrels, this.obstacleLayer);
         this.game.physics.arcade.collide(this.playerGroup, this.obstacleLayer);
 
-        this.game.physics.arcade.overlap(this.playerGroup, this.baseGroup, this.playerEntersBaseWithFlag, null, this);
+        this.game.physics.arcade.collide(this.playerGroup, this.baseGroup, this.playerEntersBaseWithFlag, null, this);
         this.game.physics.arcade.collide(this.playerGroup, this.treasureChests, this.collectTreasure, null, this);
     }
 
